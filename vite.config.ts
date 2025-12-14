9724ef1b8f0dd2cae6a354ffff0eff0e2d20dc12
VNZray <rclores666@gmail.com>
2025-12-14 14:35:51 +0800

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/great-books-sonne/',
+  base: '/great-books-sonnet/',
 })
